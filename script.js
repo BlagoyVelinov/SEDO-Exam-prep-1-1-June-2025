c654c802b7e76a291df0fe9f4a1d3999a62afc77
Blagoy <blagoy1234@gmail.com>
2025-06-20 15:32:50 +0300

    Repo init

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ function subtract(a, b) {
   return a - b;
 }
 
-function multiply(a, b) {
-  return a * b;
-}
-
 document.getElementById('btn-add').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
@@ -20,10 +16,4 @@ document.getElementById('btn-subtract').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = subtract(a, b);
-});
-
-document.getElementById('btn-multiply').addEventListener('click', () => {
-  const a = Number(document.getElementById('a').value);
-  const b = Number(document.getElementById('b').value);
-  document.getElementById('result').textContent = multiply(a, b);
 });
